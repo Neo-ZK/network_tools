8e003c13498368bc22692aff39ac0c0a6dad3d3b
毅丝 <william.zk@antfin.com>
2021-05-26 17:56:08 +0800

    add more feature

--- mock-server/python_udp_server.py
+++ mock-server/python_udp_server.py
@@ -3,6 +3,8 @@
 
 import socket
 import logging
+import struct
+import time
 
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -14,10 +16,18 @@ print('Bind UDP on 5002...')
 LOG_FORMAT = "%(asctime)s - %(levelname)s - %(message)s"
 logging.basicConfig(filename='server.log', level=logging.DEBUG, format=LOG_FORMAT)
 
+def byte42int(str):
+    arr = struct.unpack('>BBBB', str)
+    res = (arr[0] << 24) | (arr[1] << 16) |(arr[2] << 8) |arr[3]
+    return res
+
 while True:
     # 接收数据:
     data, addr = s.recvfrom(1024)
-    print('Received from %s, len is %d' % (addr, len(data)))
-    logging.debug('Received from %s, len is %d' % (addr, len(data)))
     if data:
+        seq = byte42int(data[0:4])
+        rt =  time.time() - byte42int(data[4:8])
+        print(rt)
+        print('Received from %s, len is %d, seq is %d, rt is %f' % (addr, len(data), seq, rt))
+        logging.debug('Received from %s, len is %d, seq is %d, rt is %f' % (addr, len(data), seq, rt))
         sent = s.sendto(data, addr)
